5db48dc5d6af52ac56e33bfc772b01d6a48b2c3e
CasperJSUSUDX <casper.tw.ma@gmail.com>
2026-07-13 18:21:27 +0800

    Add trigger in game

--- src/main/java/com/JSUSHDX/WorldTriggerMod/WorldTriggerMod.java
+++ src/main/java/com/JSUSHDX/WorldTriggerMod/WorldTriggerMod.java
@@ -1,5 +1,6 @@
 package com.JSUSHDX.WorldTriggerMod;
 
+import com.JSUSHDX.WorldTriggerMod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -48,6 +49,8 @@ public class WorldTriggerMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -61,7 +64,9 @@ public class WorldTriggerMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
+            event.accept(ModItems.TRIGGER);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/JSUSHDX/WorldTriggerMod/item/ModItems.java
@@ -0,0 +1,17 @@
+package com.JSUSHDX.WorldTriggerMod.item;
+
+import com.JSUSHDX.WorldTriggerMod.WorldTriggerMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(WorldTriggerMod.MODID);
+
+    public static final DeferredItem<Item> TRIGGER = ITEMS.registerSimpleItem("trigger", Item.Properties::new);
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
