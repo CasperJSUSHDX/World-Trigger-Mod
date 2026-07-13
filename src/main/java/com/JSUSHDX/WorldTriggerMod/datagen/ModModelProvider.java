258b120466d2046efb2d8400245cd3c4c26a0ac2
CasperJSUSUDX <casper.tw.ma@gmail.com>
2026-07-13 20:30:53 +0800

    Implemented auto generator of models and  items

---
+++ src/main/java/com/JSUSHDX/WorldTriggerMod/datagen/ModModelProvider.java
@@ -0,0 +1,21 @@
+package com.JSUSHDX.WorldTriggerMod.datagen;
+
+import com.JSUSHDX.WorldTriggerMod.WorldTriggerMod;
+import com.JSUSHDX.WorldTriggerMod.item.ModItems;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.model.ModelTemplate;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.data.PackOutput;
+
+public class ModModelProvider extends ModelProvider {
+    public ModModelProvider(PackOutput output) {
+        super(output, WorldTriggerMod.MODID);
+    }
+
+    @Override
+    protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
+        itemModels.generateFlatItem(ModItems.TRIGGER.get(), ModelTemplates.FLAT_ITEM);
+    }
+}
