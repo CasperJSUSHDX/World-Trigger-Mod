2f00f9033ef461907e0adfd3bcbc8735ae7051a4
Casper <casper.tw.ma@gmail.com>
2026-08-06 23:43:50 +0800

    Implement place bullets and shoot with "V" feature

--- src/main/java/com/JSUSHDX/WorldTriggerMod/item/custom/AsteroidTriggerItem.java
+++ src/main/java/com/JSUSHDX/WorldTriggerMod/item/custom/AsteroidTriggerItem.java
@@ -20,16 +20,16 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import com.JSUSHDX.WorldTriggerMod.WorldTriggerMod;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 @EventBusSubscriber(modid = WorldTriggerMod.MODID)
 public class AsteroidTriggerItem extends Item {
 
     private static final Map<UUID, FiringState> activeShooters = new ConcurrentHashMap<>();
+    private static final Map<UUID, List<ModTrionBullet>> placedBulletsMap = new ConcurrentHashMap<>();
 
     private static class FiringState {
         int bulletsLeft;
@@ -45,18 +45,21 @@ public class AsteroidTriggerItem extends Item {
         super(properties);
     }
 
-    public record modeMethods(String langId, Consumer<Void> feature) {};
+    public record modeMethods(String langId, BiConsumer<Level, Player> method) {};
     public static final Map<Integer, modeMethods> modeMap = new HashMap<>(Map.of(
-            0, new modeMethods("tooltip.wtmod.asteroid_mode0", (Void)->{}),
-            4, new modeMethods("tooltip.wtmod.asteroid_mode4", (Void)->{})
+            0, new modeMethods("tooltip.wtmod.asteroid_mode0", (level, player)->{
+                // Start firing 8 bullets, 1 immediate, 7 more to come, interval of 4 ticks (0.2s)
+                shootBullet(level, player);
+                activeShooters.put(player.getUUID(), new FiringState(7, 2));
+            }),
+            4, new modeMethods("tooltip.wtmod.asteroid_mode4", AsteroidTriggerItem::placeBullets)
     ));
 
     @Override
     public InteractionResult use(Level level, Player player, InteractionHand hand) {
         if (!level.isClientSide()) {
-            // Start firing 8 bullets, 1 immediate, 7 more to come, interval of 4 ticks (0.2s)
-            shootBullet(level, player);
-            activeShooters.put(player.getUUID(), new FiringState(7, 2));
+            BiConsumer<Level, Player> method = modeMap.get(player.getMainHandItem().get(ModDataComponents.MODE)).method;
+            method.accept(level, player);
         }
 
         return InteractionResult.SUCCESS;
@@ -80,6 +83,38 @@ public class AsteroidTriggerItem extends Item {
                     activeShooters.remove(playerId);
                 } else {
                     state.ticksUntilNext = 4; // Reset timer to 4 ticks (0.2 seconds)
+                }
+            }
+        }
+    }
+
+    public static void placeBullets(Level level, Player player) {
+        RandomSource random = level.getRandom();
+        Vec3 bulletPos = player.position().add(0, 0.5, 0);
+        for (int i = 0; i < 8; ++i) {
+            ModTrionBullet bullet = new ModTrionBullet(ModEntities.TRION_BULLET.get(), level);
+            Vec3 randomOffset = new Vec3(random.nextDouble() * 0.5, random.nextDouble() * 0.5, random.nextDouble() * 0.5);
+            bullet.setupStats(20.0d, 0.0d, 0.7d, 2.5d);
+
+            bullet.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0f, 0.0f, 1.0f);
+            bullet.setPos(bulletPos.add(randomOffset));
+
+            level.addFreshEntity(bullet);
+
+            placedBulletsMap.computeIfAbsent(player.getUUID(), k -> new ArrayList<>()).add(bullet);
+        }
+    }
+
+    public static void triggerPlacedBullets(Player player) {
+        UUID playerId = player.getUUID();
+        List<ModTrionBullet> bullets = placedBulletsMap.get(playerId);
+
+        if (bullets != null) {
+            for (ModTrionBullet bullet : bullets) {
+                if (!bullet.isRemoved()) {
+                    Vec3 look = bullet.getLookAngle();
+
+                    bullet.shoot(look.x, look.y, look.z, 1.5f, 1.0f);
                 }
             }
         }
--- src/main/java/com/JSUSHDX/WorldTriggerMod/network/CommonPayload.java
+++ src/main/java/com/JSUSHDX/WorldTriggerMod/network/CommonPayload.java
@@ -2,11 +2,13 @@ package com.JSUSHDX.WorldTriggerMod.network;
 
 import com.JSUSHDX.WorldTriggerMod.WorldTriggerMod;
 import com.JSUSHDX.WorldTriggerMod.data.ModDataComponents;
+import com.JSUSHDX.WorldTriggerMod.item.custom.AsteroidTriggerItem;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
 public class CommonPayload {
@@ -35,4 +37,27 @@ public class CommonPayload {
             return TYPE;
         }
     };
+
+    public record TriggerPlacedBullet() implements CustomPacketPayload {
+        public static final TriggerPlacedBullet INSTANCE = new TriggerPlacedBullet();
+
+        // Payload ID
+        public static final CustomPacketPayload.Type<TriggerPlacedBullet> TYPE =
+                new CustomPacketPayload.Type<>(Identifier.fromNamespaceAndPath(WorldTriggerMod.MODID, "trigger_placed_bullet"));
+
+        public static final StreamCodec<ByteBuf, TriggerPlacedBullet> STREAM_CODEC = StreamCodec.unit(INSTANCE);
+
+        public static void handler(final TriggerPlacedBullet data, final IPayloadContext context) {
+            context.enqueueWork(() -> {
+                var player = context.player();
+
+                AsteroidTriggerItem.triggerPlacedBullets(player);
+            });
+        }
+
+        @Override
+        public Type<? extends CustomPacketPayload> type() {
+            return TYPE;
+        }
+    }
 }
