3e2fe856b32f35924dde812401b8a0c1f9754616
Casper <casper.tw.ma@gmail.com>
2026-08-06 21:57:40 +0800

    Establish network basic

---
+++ src/main/java/com/JSUSHDX/WorldTriggerMod/network/ModNetwork.java
@@ -0,0 +1,17 @@
+package com.JSUSHDX.WorldTriggerMod.network;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+
+@EventBusSubscriber
+public class ModNetwork {
+    @SubscribeEvent
+    public static void register(RegisterPayloadHandlersEvent event) {
+        final PayloadRegistrar registrar = event.registrar("1.0");
+
+        // Common Payload
+        registrar.playToServer(CommonPayload.ChangeMode.TYPE, CommonPayload.ChangeMode.STREAM_CODEC, CommonPayload.ChangeMode::handler);
+    }
+}
