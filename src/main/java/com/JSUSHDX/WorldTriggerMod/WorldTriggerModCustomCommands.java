d99ddf96298b0e62107f57d4c79f1cc9d074d8e4
Casper <casper.tw.ma@gmail.com>
2026-08-08 01:41:23 +0800

    Add testing commands

--- src/main/java/com/JSUSHDX/WorldTriggerMod/WorldTriggerModCustomCommands.java
+++ src/main/java/com/JSUSHDX/WorldTriggerMod/WorldTriggerModCustomCommands.java
@@ -12,39 +12,65 @@ public class WorldTriggerModCustomCommands {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(
                 Commands.literal("wt")
-                        .then(Commands.literal("trigger")
-                                .then(Commands.literal("on")
-                                        .executes(context -> executeTrigger(context.getSource(), true))
+                        .then(Commands.literal("trion")
+                                .then(Commands.literal("get")
+                                        .executes(context -> executeTrionGet(context.getSource()))
                                 )
-                                .then(Commands.literal("off")
-                                        .executes(context -> executeTrigger(context.getSource(), false))
+                                .then(Commands.literal("add")
+                                        .then(Commands.argument("amount", com.mojang.brigadier.arguments.FloatArgumentType.floatArg())
+                                                .executes(context -> executeTrionAdd(context.getSource(), com.mojang.brigadier.arguments.FloatArgumentType.getFloat(context, "amount")))
+                                        )
+                                )
+                                .then(Commands.literal("consume")
+                                        .then(Commands.argument("amount", com.mojang.brigadier.arguments.FloatArgumentType.floatArg())
+                                                .executes(context -> executeTrionConsume(context.getSource(), com.mojang.brigadier.arguments.FloatArgumentType.getFloat(context, "amount")))
+                                        )
                                 )
                         )
         );
     }
 
-    public static int executeTrigger(CommandSourceStack source, Boolean enable) {
-        ServerPlayer player;
-
+    public static int executeTrionGet(CommandSourceStack source) {
         try {
-            player = source.getPlayerOrException();
+            ServerPlayer player = source.getPlayerOrException();
+            float trion = com.JSUSHDX.WorldTriggerMod.util.TrionUtils.getTrion(player);
+            float maxTrion = com.JSUSHDX.WorldTriggerMod.util.TrionUtils.getMaxTrion(player);
+            player.sendSystemMessage(Component.literal("§b[WT] 當前觸力能: " + trion + " / " + maxTrion));
+            return 1;
         } catch (Exception e) {
-            source.sendFailure(Component.literal("Can only be execute by player"));
+            source.sendFailure(Component.literal("Only players can use this command."));
             return 0;
         }
+    }
 
-        if (!enable) {
-            player.sendSystemMessage(Component.literal("§a[WT] " + player.getPlainTextName() + " Trigger off!"));
+    public static int executeTrionAdd(CommandSourceStack source, float amount) {
+        try {
+            ServerPlayer player = source.getPlayerOrException();
+            com.JSUSHDX.WorldTriggerMod.util.TrionUtils.addTrion(player, amount);
+            float current = com.JSUSHDX.WorldTriggerMod.util.TrionUtils.getTrion(player);
+            player.sendSystemMessage(Component.literal("§a[WT] 增加 " + amount + " 觸力能。當前: " + current));
             return 1;
+        } catch (Exception e) {
+            source.sendFailure(Component.literal("Only players can use this command."));
+            return 0;
         }
+    }
 
-        ItemStack mainHandItem = player.getMainHandItem();
-
-        if (mainHandItem.is(ModItems.TRIGGER.get())) {
-            player.sendSystemMessage(Component.literal("§a[WT] " + player.getPlainTextName() + " Trigger on!"));
-            return 1;
-        } else {
-            player.sendSystemMessage(Component.literal("§c[WT] " + player.getPlainTextName() + " fail to use Trigger."));
+    public static int executeTrionConsume(CommandSourceStack source, float amount) {
+        try {
+            ServerPlayer player = source.getPlayerOrException();
+            boolean success = com.JSUSHDX.WorldTriggerMod.util.TrionUtils.consumeTrion(player, amount);
+            if (success) {
+                float current = com.JSUSHDX.WorldTriggerMod.util.TrionUtils.getTrion(player);
+                player.sendSystemMessage(Component.literal("§e[WT] 成功消耗 " + amount + " 觸力能。剩餘: " + current));
+                return 1;
+            } else {
+                float current = com.JSUSHDX.WorldTriggerMod.util.TrionUtils.getTrion(player);
+                player.sendSystemMessage(Component.literal("§c[WT] 觸力能不足！需要: " + amount + "，目前只有: " + current));
+                return 0;
+            }
+        } catch (Exception e) {
+            source.sendFailure(Component.literal("Only players can use this command."));
             return 0;
         }
     }
